03637a4ef910f2575ec19574aca5684d6a95dd82
amardeep <amardeepjk@gmail.com>
2024-03-09 09:30:43 +0800

    wip

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "vector"
+int main() {
+
+    int t = 56;
+
+    const std::size_t A = static_cast<std::size_t>(1e9);
+    const std::size_t B = static_cast<std::size_t>(1e9);
+    const std::vector<std::size_t> ALLOCATION_SIZES {32, 64, 256, 512, 1024, 2048, 4096};
+    const std::vector<std::size_t> ALIGNMENTS {8, 8, 8, 8, 8, 8, 8};
+
+    Allocator * cAllocator = new CAllocator();
+
+}
