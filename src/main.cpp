4c7c3eb680a2d7cdcb2218e0cc0ebc03e2fb8842
amardeep <amardeepjk@gmail.com>
2024-03-17 11:40:29 +0800

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,18 @@
 #include "Allocator.h"
 #include "CAllocator.h"
 #include "Benchmark.h"
+
 int main() {
 
     int t = 56;
 
     const std::size_t A = static_cast<std::size_t>(1e9);
     const std::size_t B = static_cast<std::size_t>(1e9);
-    const std::vector<std::size_t> ALLOCATION_SIZES {32, 64, 256, 512, 1024, 2048, 4096};
-    const std::vector<std::size_t> ALIGNMENTS {8, 8, 8, 8, 8, 8, 8};
+    const std::vector<std::size_t> ALLOCATION_SIZES{32, 64, 256, 512, 1024, 2048, 4096};
+    const std::vector<std::size_t> ALIGNMENTS{8, 8, 8, 8, 8, 8, 8};
 
-    Allocator * cAllocator = new CAllocator();
+    Allocator *cAllocator = new CAllocator();
 
-
+    Benchmark benchmark(OPERATIONS);
 
 }
